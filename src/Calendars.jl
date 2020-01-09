63a468797dc09f5df05728e8771ce28ddae64d76
WooKyoung Noh <wookay.noh@gmail.com>
2020-01-09 20:06:51 +0900

    LunarCalendars

--- src/Calendars.jl
+++ src/Calendars.jl
@@ -2,5 +2,6 @@ module Calendars
 
 include("VerticalCalendar.jl")
 include("Times.jl")
+include("LunarCalendars.jl")
 
 end # module Calendars
---
+++ test/calendars/lunarcalendars.jl
@@ -0,0 +1,22 @@
+module test_calendars_lunarcalendars
+
+using Test
+using Calendars.LunarCalendars
+
+april23 = Date(2020, 4, 23)
+lunar_april23 = Lunar(april23)
+@test lunar_april23 == Lunar(2020, 4, 1, false)
+@test lunar_to_solar(lunar_april23) == april23
+
+# intercalation
+may23 = Date(2020, 5, 23)
+lunar_may23 = Lunar(may23)
+@test lunar_may23 == Lunar(2020, 4, 1, true)
+@test lunar_to_solar(lunar_may23) == may23
+
+solar = Date(2020, 1, 9)
+lunar = Lunar(solar)
+@test solar_to_lunar(solar) == lunar
+@test lunar_to_solar(lunar) == solar
+
+end # module test_calendars_lunarcalendars
